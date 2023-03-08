394a837d31639855da2ee48b6a61baa5c14b1a8b
Nur Syamsu Wais Al Qorni <51069682+nsyamsu23@users.noreply.github.com>
2023-03-08 10:59:11 +0700

    Update main.py

--- main.py
+++ main.py
@@ -55,10 +55,8 @@ def to_excel1(df):
       writer.sheets[sheet].set_row(2, 15) # Set the header row height to 15
       i = 4;
       for col_num, value in enumerate(df[["NAMA LENGKAP","KELAS","NILAI"]].columns.values):
-          writer.sheets[sheet].write(4,0, 'No', header_format)
-          writer.sheets[sheet].write(4, col_num+1,value,header_format)
+          writer.sheets[sheet].write(4, col_num,value,header_format)
           # Adjust the column width.
-          writer.sheets[sheet].write_column(5, 0, col_num+1)
           writer.sheets[sheet].set_column('A:A', 15,col1_format)
           writer.sheets[sheet].set_column('B:B', 40,col1_format)
           writer.sheets[sheet].set_column('C:D', 15,col_format)
