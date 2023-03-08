7dda052d3c6a250f7adb845d6ad759ed389e9532
Nur Syamsu Wais Al Qorni <51069682+nsyamsu23@users.noreply.github.com>
2023-03-08 10:14:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ def to_excel1(df):
       writer.sheets[sheet].merge_range('A3:C3', subheader1_new,format)
       writer.sheets[sheet].set_row(2, 15) # Set the header row height to 15
       for col_num, value in enumerate(df[["NAMA LENGKAP","KELAS","NILAI"]].columns.values):
-          writer.sheets[sheet].write(4,0, 'No',col_num+1, header_format)
+          writer.sheets[sheet].write(4,0, 'No',value, header_format)
           writer.sheets[sheet].write(4, col_num+1, value, header_format)
           # Adjust the column width.
           writer.sheets[sheet].set_column('A:A', 15,col1_format)
