3268c8d05289162ca00b1532afc47c87d9d68d22
Nur Syamsu Wais Al Qorni <51069682+nsyamsu23@users.noreply.github.com>
2023-03-08 10:40:00 +0700

    Update main.py

--- main.py
+++ main.py
@@ -54,8 +54,7 @@ def to_excel1(df):
       i = 4;
       for col_num, value in enumerate(df[["NAMA LENGKAP","KELAS","NILAI"]].columns.values):
           writer.sheets[sheet].write(4,0, 'No', header_format)
-          writer.sheets[sheet].write(i+4,0, col_num+1, header_format)
-          writer.sheets[sheet].write(4, col_num+1,value,header_format)
+          writer.sheets[sheet].write(i+1,0, col_num+1, header_format)
           # Adjust the column width.
           writer.sheets[sheet].set_column('A:A', 15,col1_format)
           writer.sheets[sheet].set_column('B:B', 40,col1_format)
