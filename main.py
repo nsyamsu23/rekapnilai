7e438ec1a38e4a2f91aea2a5b98866b7be3f63aa
Nur Syamsu Wais Al Qorni <51069682+nsyamsu23@users.noreply.github.com>
2023-03-08 08:47:23 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from xlsxwriter.utility import xl_rowcol_to_cell
 def to_excel1(df):
     df = df[[df.columns[2],"Score","Kelas"]]
     df = df.rename(columns={df.columns[0] : 'Nama', 'Score': 'Nilai'})
+    df = df.sort_values(['Nama'], ascending=[True])
     df.reset_index(drop=True)
     df1 = df.groupby('Kelas').agg({"count"})
     df1 = df1.reset_index()
@@ -25,7 +26,7 @@ def to_excel1(df):
       #add title
       title_new = title
       subheader_new = subheader
-      subheader1 = "KELAS "+sheet
+      subheader1_new = subheader1
       #merge cells
 
       format = workbook.add_format({
@@ -46,9 +47,9 @@ def to_excel1(df):
       
       writer.sheets[sheet].merge_range('A1:C1', title_new, format)
       writer.sheets[sheet].merge_range('A2:C2', subheader_new,format)
-      writer.sheets[sheet].merge_range('A3:C3', subheader1,format)
+      writer.sheets[sheet].merge_range('A3:C3', subheader1_new,format)
       writer.sheets[sheet].set_row(2, 15) # Set the header row height to 15
-      for col_num, value in enumerate(df[["Nama","Nilai","Kelas"]].columns.values):
+      for col_num, value in enumerate(df[["Nama","Kelas","Nilai"]].columns.values):
           
           writer.sheets[sheet].write(4, col_num, value, header_format)
           # Adjust the column width.
@@ -59,9 +60,10 @@ def to_excel1(df):
     return processed_data
 
 st.write('# REKAP NILAI')
-title =st.text_input('Judul', 'NILAI SEMESTER GANJIL 2022 - 2023')
+title =st.text_input('Judul', 'REKAPITULASI')
 
 subheader =st.text_input('SubJudul', 'PENDIDIKAN AGAMA ISLAM')
+subheader1 =st.text_input('SubJudul', 'PTS GANJIL 2022/2023')
 
 uploaded_file = st.file_uploader("Upload spreadsheet", type=["csv", "xlsx"])
 
