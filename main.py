6086026a30fa6d7c94c2cd82440389c46f8ae820
Nur Syamsu Wais Al Qorni <51069682+nsyamsu23@users.noreply.github.com>
2023-03-13 13:15:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import xlsxwriter
 from io import BytesIO
 from xlsxwriter.utility import xl_rowcol_to_cell
 def to_excel1(df):
-    df = df[[df.columns[2],df.columns[1],df.columns[3]]
+    df = df[[df.columns[2],df.columns[1],df.columns[3]]]
     df = df.rename(columns={df.columns[0] : 'NAMA LENGKAP', df.columns[2]: 'KELAS',df.columns[1]: 'NILAI'})
+    
     df['NAMA LENGKAP']= df['NAMA LENGKAP'].str.upper().str.title()
     df = df.sort_values(['NAMA LENGKAP'], ascending=[True])
     df.reset_index(drop=True)
