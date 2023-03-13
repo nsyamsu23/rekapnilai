e4736a633f53d2477a4279a980a16a65c6a83cbf
Nur Syamsu Wais Al Qorni <51069682+nsyamsu23@users.noreply.github.com>
2023-03-13 14:41:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -18,9 +18,6 @@ def to_excel1(df):
     for sheet in name_sheet:
        numbers = []
        df[df["KELAS"]== str(sheet)].to_excel(writer, sheet_name=str(sheet),index=False,startcol=0,startrow=4)
-       if(df["KELAS"]== sheet):
-           for i in range(1, len(df['NAMA LENGKAP'])+1):
-                    numbers.append(i)
     workbook = writer.book
     header_format = workbook.add_format({
             "valign": "vcenter",
@@ -56,13 +53,15 @@ def to_excel1(df):
       writer.sheets[sheet].merge_range('A2:C2', subheader_new,format)
       writer.sheets[sheet].merge_range('A3:C3', subheader1_new,format)
       writer.sheets[sheet].set_row(2, 15) # Set the header row height to 15
+      my_list = [1, 2, 3, 4, 5]
 
-      for col_num, value in enumerate(df[["NO","NAMA LENGKAP","KELAS","NILAI"]].columns.values):
-          writer.sheets[sheet].write(4, col_num,value,header_format)
+      for row_num, data in enumerate(my_list):
+           worksheet.write(row_num, 0, data)
+      for col_num, value in enumerate(df[["NAMA LENGKAP","KELAS","NILAI"]].columns.values):
+          writer.sheets[sheet].write(4, col_num+1,value+1,header_format)
           # Adjust the column width.
-          writer.sheets[sheet].set_column('A:A', 15,col_format)
-          writer.sheets[sheet].set_column('B:B', 40,col1_format)
-          writer.sheets[sheet].set_column('C:D', 15,col_format)
+          writer.sheets[sheet].set_column('A:A', 40,col1_format)
+          writer.sheets[sheet].set_column('B:D', 15,col_format)
       writer.sheets[sheet].conditional_format(xlsxwriter.utility.xl_range(4, 0, 4+len(df[df["KELAS"]== str(sheet)]), len(df[df["KELAS"]== str(sheet)].columns) - 1), {'type': 'no_errors'})
     writer.save()
     processed_data = output.getvalue()
