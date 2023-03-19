d2c974209c9b81704880da345c78a0c21a165d78
Nur Syamsu Wais Al Qorni <51069682+nsyamsu23@users.noreply.github.com>
2023-03-19 20:40:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ if uploaded_file:
     df_xlsx = to_excel1(df)
     st.download_button(label='📥 Download Current Result',
                                 data=df_xlsx ,
-                                file_name= subheader+'.xlsx')
+                                file_name= 'NILAI '+subheader+'.xlsx')
 
 
 
